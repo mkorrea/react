50c078e535aa132441928e90cac66531ab4a8eb0
mkorrea <mathscorrea@gmail.com>
2024-01-09 17:36:58 -0300

    funções

---
+++ aulas/seção 2/9. funções/script.js
@@ -0,0 +1,22 @@
+function entrar() {
+    let area = document.getElementById('area')
+    let name = prompt('Digite seu nome:')
+
+    if(name == '' || name == null) {
+        alert('Digite seu nome novamente.')
+    } else {
+        sobrenome(name)
+        idade()
+    }
+}
+function sobrenome(name) {
+    let nick = prompt('Digite seu sobrenome:')
+    area.innerHTML = `Olá, Seja Bem vindo ${name} ${nick}`
+}
+
+function idade() {
+    let idade = document.getElementById('idade')
+    let anos = prompt('Quantos anos você tem ?')    
+
+    idade.innerHTML = `Você tem ${anos} anos`
+}
