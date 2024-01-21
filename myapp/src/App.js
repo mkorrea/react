41159908002aae6805950dd74dc8d957a837d0f0
mkorrea <mathscorrea@gmail.com>
2024-01-20 23:01:52 -0300

    ex component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,12 @@
-export default function App () {
+function App () {
   return(
     <div>
       <h1>Olá mundo!</h1>
+      <Bemvindo/>
     </div>
   )
 }
+
+const Bemvindo = () => <h2>Bem-vindo!</h2>
+
+export default App
