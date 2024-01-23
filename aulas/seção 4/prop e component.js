d7e849417491fe8851b247d381b9543abb89bd7c
mkorrea <mathscorrea@gmail.com>
2024-01-23 20:17:13 -0300

    componetns stateless

--- aulas/seção 4/prop e component.js
+++ aulas/seção 4/prop e component.js
@@ -3,13 +3,26 @@
 //  Stateless component ( componentes funcionais )
         // Este é um componente
 const Bemvindo = () => <h2>Bem-vindo!</h2>
+        // criando uma propriedade ( props ), coloca-se um parâmetro na função
+/*const Bemvindo =*/ (props) => {
+    return (
+      <div>
+        {/* define o uso do parametro e cada prop */}
+        <h2>Bem-vindo! {props.nome}</h2>
+        <h3>Tenho {props.idade} anos</h3>
+      </div>
+    )
+}
+
 function App () {
   return(
     <div>
       <h1>Olá mundo!</h1>
       {/* chamar um componente */}
       <Bemvindo/>
+      {/* chamar um componente com as props */}
+      <Bemvindo nome="Matheus" idade="23"/>
+      <Bemvindo nome="Junior" idade="27"/>         
     </div>
   )
 }
-const meuElemento = <div>Olá, JSX!</div>;
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,21 @@
 
-const Bemvindo = () => <h2>Bem-vindo!</h2>
+const Bemvindo = (props) => {
+  return (
+    <div>
+      <h2>Bem-vindo! {props.nome}</h2>
+      <h3>Tenho {props.anos} anos</h3>
+    </div>
+  )
+}
 
 function App () {
   return(
     <div>
       <h1>Olá mundo!</h1>
-      <Bemvindo/>
+      <div>
+        <Bemvindo nome="Matheus" anos="23"/>
+        <Bemvindo nome="Junior" anos="27"/>
+      </div>
     </div>
   )
 }
