af8df3299baea2fd514c9c0ea384bb039648fc95
mkorrea <mathscorrea@gmail.com>
2024-01-25 17:31:38 -0300

    final ex card

--- exercicios/1. component/card produto/script.js
+++ exercicios/1. component/card produto/script.js
@@ -1,11 +1,14 @@
 const enun = document.getElementById('enunciado').innerText = 'Crie um componente funcional chamado CardProduto que receba props para o nome, preço e uma imagem do produto. Renderize um card simples exibindo essas informações.'
 
 
+
 const CardProduto = (prop) => {
-  let newProd = document.createElement('div.prod')
-  let newName = document;createElement('div.nome')
   return (
-    novo
+    <div class="prod">
+      <div class="nome">{prop.nome}</div>
+      <img class="img" src={prop.img} alt={prop.nome}></img>
+      <div class="preco">{prop.preco}</div>
+    </div>
   )
 }
 
@@ -15,7 +18,11 @@ const App = () => {
       <CardProduto 
       nome="Logitech G435" 
       img="https://images.kabum.com.br/produtos/fotos/263077/headset-gamer-sem-fio-logitech-g435-lightspeed-e-bluetooth-dolby-atmos-usb-pc-ps4-ps5-mobile-drivers-40mm-preto-981-001049_1636554612_gg.jpg" 
-      preco="R$449,99" />
+      preco="R$ 449,99" />
+      <CardProduto 
+      nome="Fan Rise Mode Laser" 
+      img="https://images.kabum.com.br/produtos/fotos/153646/cooler-fan-rise-mode-laser-3-unidades-120mm-argb-preto-rm-rgb-05-5v_1663767095_gg.jpg" 
+      preco="R$ 89,99" />
     </div>
   );
 };
