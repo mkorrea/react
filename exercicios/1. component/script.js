3eb349ef0532c33de46e85e5816fc5fdab52cf82
mkorrea <mathscorrea@gmail.com>
2024-01-25 09:18:32 -0300

    ex saudação component

--- exercicios/1. component/script.js
+++ exercicios/1. component/script.js
@@ -1,13 +1,25 @@
 const enun = document.getElementById('enunciado').innerText = 'Crie um componente funcional chamado Saudacao que recebe um prop nome e exibe uma saudação personalizada. Utilize-o no componente App passando diferentes nomes.'
 
 
+let nome = prompt('Qual o seu nome?')
+
+const Saudacao = (prop) => {
+  return (
+    <div>
+      Olá {prop.nome}, seja bem-vindo!
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <div>
-
+      <Saudacao nome={nome}/>
     </div>
   );
 };
+
+
 
 ReactDOM.render(
   <App />,
