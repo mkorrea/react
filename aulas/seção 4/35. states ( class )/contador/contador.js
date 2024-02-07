746ca13bac649b983d8567496ea1337d64a81ab5
mkorrea <mathscorrea@gmail.com>
2024-02-07 01:18:35 -0300

    explicação

--- aulas/seção 4/35. states ( class )/contador/contador.js
+++ aulas/seção 4/35. states ( class )/contador/contador.js
@@ -8,17 +8,20 @@ class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      contador: 0
+      contador: 0,
     }
 
     this.diminuir = this.diminuir.bind(this)
     this.aumentar = this.aumentar.bind(this)
+    // bind() é usado para VINCULAR algo, nesse caso, está usando o "this.diminuir/aumentar para vincular ao constructor.   bind(this) = vincular(a este local)"
   }
 
   aumentar(){
     let state = this.state
+    // criar uma variável para o estado
     state.contador += 1
     this.setState(state)
+    // re-configura e atualiza o estado/valor
   }
 
   diminuir(){
