cfe51fc1774986b0ab77848578ea41a6ca9b79ef
mkorrea <mathscorrea@gmail.com>
2024-02-23 17:38:54 -0300

    add aula sobre eventos, importar arquivo react

--- aulas/seção 5/39. eventos/aula.js
+++ aulas/seção 5/39. eventos/aula.js
@@ -2,10 +2,23 @@
 
 */
 
+import React, { Component } from 'react';
+import Membro from './cadastro/membro'
+
+class App extends Component {
 
 
+  render(){
+    return(
+      <div>
+        <h1> <Membro nome="Visitante"/> </h1>
+      </div>
+    )
+  }
+}
 
 
+export default App
 
 
 ReactDOM.render(
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
     return(
       <div>
-        <h1> <Membro nome="Silva"/> </h1>
+        <h1> <Membro nome="Visitante"/> </h1>
       </div>
     )
   }
