647a5df3ef3f4661b2e9049dad01e10d4076ea32
mkorrea <mathscorrea@gmail.com>
2024-03-06 14:31:03 -0300

    att confirmação de formulario react

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,13 +7,18 @@ class App extends Component {
          nome: "",
          email: "",
          senha: "",
+         error: ''
       };
       this.cadastro = this.cadastro.bind(this)
    }
 
    cadastro(event){
       const {nome, email, senha} = this.state;
-      alert(`nome: ${nome} \nemail: ${email} \nsenha: ${senha}`);
+      if (nome != '' && email != '' && senha != ''){
+         alert(`nome: ${nome} \nemail: ${email} \nsenha: ${senha}`);
+      } else {
+         this.setState({error: 'Verifique novamente os campos'})
+      }
 
       event.preventDefault();
       // previne da pagina atualizar ao clicar em cadastrar
@@ -23,7 +28,7 @@ class App extends Component {
       return (
          <div>
             <h1> Formulário </h1>
-
+            {this.state.error && <p>{this.state.error}</p>}
             <form onSubmit={this.cadastro}>
                Nome:
                <input
