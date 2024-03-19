4cf6ee524a48907eeffe0c44d3ae77069b30fa23
mkorrea <mathscorrea@gmail.com>
2024-03-18 22:16:00 -0300

    useEffect

---
+++ aulas/secão 6/48. useState/aula.js
@@ -0,0 +1,33 @@
+//  useState
+
+import React, { useState } from "react";
+
+function App () {
+   
+   const [toDo, setToDo] = useState([
+      'trabalhar',
+      'estudar react'
+   ])
+   
+   const [input, setInput] = useState('')
+
+   function addTask () {
+      setToDo([...toDo, input])
+      setInput('')
+   }
+
+   return(
+      <div>
+         <ul>
+            {toDo.map(task => (
+               <li key={task}> {task} </li>
+            ))}
+         </ul>
+
+         <input type="text" value={input} onChange={e => setInput(e.target.value)}></input>
+         <button type="button" onClick={addTask}> Submit </button>
+      </div>
+   )
+}
+
+export default App;
