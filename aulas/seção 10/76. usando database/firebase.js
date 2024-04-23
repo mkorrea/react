2cca2930f763e53031107be134a228e175fe6a44
mkorrea <mathscorrea@gmail.com>
2024-04-23 12:24:25 -0300

    resgatar todos os docs do banco de dados

--- aulas/seção 10/76. usando database/firebase.js
+++ aulas/seção 10/76. usando database/firebase.js
@@ -1,26 +1,61 @@
 import { useState } from 'react'
 import { db } from './firebaseConnections'
-import { doc, setDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
 import './app.css'
 
 function App() {
    const [ nome, setNome ] = useState('')
    const [ idade, setIdade ] = useState('')
+
    
    async function handleAdd() {
-      await setDoc(doc( db, 'users', '3' ), {
+/*
+         await setDoc(doc( db, 'users', '3' ), {
+            nome: nome,
+            idade: idade,
+         })
+         .then(()=>{
+            console.log("Dados registrados")
+         })
+         .catch((erro)=>{
+            console.log("Erros nos dados: " + erro)
+         })
+// Adiciona os dados de nome e idade (dos inputs) á coleção "users" na minha conta do firebase
+// dentro do documento 3 (de maneira, com id fixo)
+*/
+      await addDoc(collection(db, "users"), {
          nome: nome,
          idade: idade,
       })
       .then(()=>{
-         console.log("Dados registrados")
+         alert("Dados registrados")
+         setNome('')
+         setIdade('')
       })
       .catch((erro)=>{
-         console.log("Erros nos dados: " + erro)
+         alert("Erros nos dados: " + erro)
       })
-   }
-   // Adiciona os dados de nome e idade (dos inputs) á coleção "users" na minha conta do firebase
-   // dentro do documento 3 (de maneira, com id fixo)
+      // Armazenamento de dados no firebase com id aleatório, por causa do "addDoc" e "collection"
+      }
+   
+
+      // Buscar por dado especifico no banco de dados:
+      async function resgatarUser() {
+
+         const userRef = doc(db, "users", '2')
+
+         await getDoc(userRef)
+         // ou: 
+         //await getDoc(doc(db, "users", "2"))
+         .then((snapshot)=>{
+            setNome(snapshot.data().nome)
+            setIdade(snapshot.data().idade)
+         })
+         .catch(()=>{
+            alert("erro")
+         })
+      }
+
 
   return (
     <div>
@@ -35,7 +70,9 @@ function App() {
          <input type='number' value={idade} onChange={(e)=>setIdade(e.target.value)}/>
 
          <button onClick={handleAdd}>Cadastrar</button>
+         <button onClick={resgatarUser}>Resgatar dado</button>
       </div>
+
     </div>
   );
 }
